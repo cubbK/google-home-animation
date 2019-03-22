d41d7e4d43eb08be1cb905cab233ab2a952e026c
cubbk <danpopa1998@gmail.com>
2019-03-22 21:45:32 +0200

    add click animation

--- src/components/phone/Bar.js
+++ src/components/phone/Bar.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import backIcon from "./bar/back-icon.png";
 import homeIcon from "./bar/home-icon.png";
 import menuIcon from "./bar/menu-icon.png";
+import { useSpring, animated, config } from "react-spring";
+import { easeExpOut, easeElasticIn } from "d3-ease"
 
 const Container = styled.div`
   width: calc(100% - 140px);
@@ -16,7 +18,8 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Button = styled.div`
+const ButtonDiv = styled.div`
+  position: relative;
   height: 100%;
   width: 80px;
   border-radius: 20px;
@@ -34,18 +37,45 @@ const Button = styled.div`
   }
 `;
 
+const ButtonOutline = styled(animated.div)`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.3);
+`;
+
+function Button(props) {
+  const [outlineStyles, setOutlineStyles] = useSpring(() => ({
+    from: { width: "40%", opacity: 0 },
+    config: { easing: t => easeElasticIn(t), tension: 550 }
+  }));
+
+  function triggerAnimation() {
+    setOutlineStyles({
+      to: async next => {
+        await next({ opacity: 1, width: "100%" });
+        await next({ opacity: 0, width: "40%" });
+      }
+    });
+  }
+
+  return (
+    <ButtonDiv onClick={triggerAnimation}>
+      <img alt="" {...props} />
+      <ButtonOutline style={outlineStyles} />
+    </ButtonDiv>
+  );
+}
+
 export default function Bar(props) {
   return (
     <Container>
-      <Button>
-        <img src={backIcon} alt="back" />
-      </Button>
-      <Button>
-        <img src={homeIcon} alt="home" />
-      </Button>
-      <Button>
-        <img src={menuIcon} alt="menu" />
-      </Button>
+      <Button src={backIcon} alt="back" />
+
+      <Button src={homeIcon} alt="home" />
+
+      <Button src={menuIcon} alt="menu" />
     </Container>
   );
 }
