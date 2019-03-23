9ec679a128f491826214f4da6c2cb89c8cbc4e9a
cubbk <danpopa1998@gmail.com>
2019-03-23 13:02:17 +0200

    add static google dots

--- src/components/phone/bar/HomeButton.js
+++ src/components/phone/bar/HomeButton.js
@@ -4,6 +4,13 @@ import Button from "./Button";
 import { useSpring, animated } from "react-spring";
 import ClickNHold from "react-click-n-hold";
 
+const googleColors = {
+  red: "#DB4437",
+  blue: "#4285F4",
+  yellow: "#F4B400",
+  green: "#0F9D58"
+}
+
 const CircleOuter = styled(animated.div)`
   position: absolute;
   width: 14px;
@@ -20,9 +27,28 @@ const CircleInner = styled(animated.div)`
   height: 11px;
   border-radius: 50%;
   background-color: #fff;
+  z-index: 1;
 `;
 
+const GoogleDot = styled(animated.div)`
+  position: absolute;
+  border-radius: 50%;
+  width: 7px;
+  height: 7px;
+`
 
+const GoogleDotRed = styled(GoogleDot)`
+  background-color: ${googleColors.red};
+`
+const GoogleDotBlue = styled(GoogleDot)`
+  background-color: ${googleColors.blue};
+`
+const GoogleDotYellow = styled(GoogleDot)`
+  background-color: ${googleColors.yellow};
+`
+const GoogleDotGreen = styled(GoogleDot)`
+  background-color: ${googleColors.green};
+`
 
 export default function HomeButton() {
   const [outerCircleStyles, setOuterCircleStyles] = useSpring(() => ({
@@ -30,12 +56,23 @@ export default function HomeButton() {
     config: { tension: 550 }
   }));
 
+  const [innerCircleStyles, setInnerCircleStyles] = useSpring(() => ({
+    from: { width: "11px", height: "11px" },
+    config: { tension: 550 }
+  }));
+
   function triggerAnimation() {
     setOuterCircleStyles({ to: { width: "0px", height: "0px" } });
+    setInnerCircleStyles({
+      to: async next => {
+        await next({ width: "8px", height: "8px" });
+      }
+    });
   }
 
   function endAnimation() {
     setOuterCircleStyles({ to: { width: "14px", height: "14px" } });
+    setInnerCircleStyles({ to: { width: "11px", height: "11px" } })
   }
 
   return (
@@ -47,7 +84,11 @@ export default function HomeButton() {
       <div>
         <Button>
           <CircleOuter style={outerCircleStyles} />
-          <CircleInner />
+          <CircleInner style={innerCircleStyles} />
+          <GoogleDotRed />
+          <GoogleDotBlue />
+          <GoogleDotYellow />
+          <GoogleDotGreen />
         </Button>
       </div>
     </ClickNHold>
